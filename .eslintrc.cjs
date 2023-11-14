3597e674f79b0e7437e3acb1abeca6aca9c10ac9
sergeynagorny <sergey.s.nagorny@gmail.com>
2023-11-14 17:26:15 +0300

    config(app): prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,11 +5,14 @@ module.exports = {
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
+    'plugin:lodash/recommended'
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  plugins: ['react-refresh', 'lodash'],
   rules: {
+    "lodash/prefer-lodash-method": "off",
+    'lodash/import-scope': [2, "method"],
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
---
+++ .prettierrc.js
@@ -0,0 +1,27 @@
+/** @type {import("prettier").Config} */
+const config = {
+  singleQuote: true,
+  bracketSpacing: true,
+  bracketSameLine: false,
+  printWidth: 120,
+  semi: true,
+  trailingComma: 'all',
+  tabWidth: 2,
+  endOfLine: 'lf',
+
+  importOrder: [
+    '^react$',
+    '<THIRD_PARTY_MODULES>',
+    '^@/pages/(.*)$',
+    '^@/widgets/(.*)$',
+    '^@/features/(.*)$',
+    '^@/entities/(.*)$',
+    '^@/shared/(.*)$',
+    '^[./]',
+  ],
+  importOrderSeparation: true,
+  importOrderSortSpecifiers: true,
+  plugins: ['@trivago/prettier-plugin-sort-imports'],
+};
+
+export default config;
