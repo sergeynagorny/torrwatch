f2b84c68b61297419ce6ee1fdae57c8d8228f7a4
sergeynagorny <sergey.s.nagorny@gmail.com>
2023-11-15 20:02:22 +0300

    feat(app): theme-provider

--- src/app.tsx
+++ src/app.tsx
@@ -1,10 +1,27 @@
-import { Button } from '@/shared/components/ui/button';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { Route, Routes } from 'react-router-dom';
+
+import { Home } from '@/pages/home';
+import { Layout } from '@/pages/layout.tsx';
+import { Torrents } from '@/pages/torrents.tsx';
+
+import { ThemeProvider } from '@/shared/components/theme-provider.tsx';
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
-    <>
-      <Button>Button</Button>
-    </>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="torrents" element={<Torrents />} />
+            <Route path="*" element={<div>Not Found</div>} />
+          </Route>
+        </Routes>
+      </ThemeProvider>
+    </QueryClientProvider>
   );
 }
 
