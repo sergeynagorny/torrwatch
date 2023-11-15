c6542342895a6b6bc3dd239ebe1402bd3d22b69d
sergeynagorny <sergey.s.nagorny@gmail.com>
2023-11-15 20:00:33 +0300

    config(vite-server): proxy qbit api

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,19 @@
+import react from '@vitejs/plugin-react';
 import path from 'path';
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/torr-api': {
+        target: 'http://localhost:8070',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/torr-api/, ''),
+      },
+    },
+  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
