e3e691469bfaf8402bc9720cf56a4366452ba2af
sergeynagorny <sergey.s.nagorny@gmail.com>
2023-11-15 19:59:00 +0300

    config(prettier): prettier-plugin-tailwindcss

--- .prettierrc.js
+++ .prettierrc.js
@@ -21,7 +21,7 @@ const config = {
   ],
   importOrderSeparation: true,
   importOrderSortSpecifiers: true,
-  plugins: ['@trivago/prettier-plugin-sort-imports'],
+  plugins: ['@trivago/prettier-plugin-sort-imports', 'prettier-plugin-tailwindcss'],
 };
 
 export default config;
