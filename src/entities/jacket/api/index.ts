8738d83a9909d0e9c5501dd46d6105cc95e6eb71
sergeynagorny <sergey.s.nagorny@gmail.com>
2023-11-17 20:00:59 +0300

    fix(jacket/api): search params

--- src/entities/jacket/api/index.ts
+++ src/entities/jacket/api/index.ts
@@ -1,19 +1,22 @@
+import axios from 'axios';
+
 import { combineJacketsByTracker } from '@/entities/jacket/api/lib.ts';
 import { Jacket } from '@/entities/jacket/types.ts';
 
 export const getJacketList = ({
   q,
   signal,
-  year = 1985,
+  year,
 }: {
   year?: number;
   q: string;
   signal?: AbortSignal;
 }): Promise<Jacket[]> => {
-  return fetch(`${import.meta.env.VITE_JACKET_API}/api/v2.0/indexers/all/results?apikey=&query=${q}&year=${year}`, {
-    signal,
-  })
-    .then((r) => r.json())
-    .then((r) => r.Results)
+  return axios
+    .get(`${import.meta.env.VITE_JACKET_API}/api/v2.0/indexers/all/results`, {
+      params: { year, query: q, apikey: null },
+      signal,
+    })
+    .then((r) => r.data.Results)
     .then(combineJacketsByTracker);
 };
